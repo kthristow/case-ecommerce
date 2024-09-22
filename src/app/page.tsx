f320ce73e5d0912515f3a753c462b5695220f938
Kaloyan <kthristow@gmail.com>
2024-09-22 08:09:36 +0300

    Add animation to the landing page
    Create components for showing a good looking animation for phone cases at the landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Check, Star } from 'lucide-react'
 import Phone from "@/components/Phone";
 import { Icons } from "@/components/Icon";
+import {Reviews} from "@/components/Reviews";
 
 export default function Home() {
   return (
@@ -137,6 +138,10 @@ export default function Home() {
             </div>
           </div>
         </MaxWidthWrapper>
+
+        <div className="pt-16">
+        <Reviews />
+        </div>
       </section>
     </div>
   );
