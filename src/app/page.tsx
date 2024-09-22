6dbbac5b4cc87fbc8d455979c2dab8f66b1c663e
Kaloyan <kthristow@gmail.com>
2024-09-22 09:30:00 +0300

    Update the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
-import { Check, Star } from 'lucide-react'
+import { ArrowRight, Check, Star } from 'lucide-react'
 import Phone from "@/components/Phone";
 import { Icons } from "@/components/Icon";
-import {Reviews} from "@/components/Reviews";
+import { Reviews } from "@/components/Reviews";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -102,11 +104,11 @@ export default function Home() {
                   , on the case I had before, the image started fading into yellow-ish color after a couple weeks. Love it."</p>
               </div>
               <div className="flex gap-4 mt-2">
-                <img className="rounded-full h-12 w-12 object-cover" src='/users/user-1.png'/>
+                <img className="rounded-full h-12 w-12 object-cover" src='/users/user-1.png' />
                 <div className="flex flex-col">
                   <p className="font-semibold">Jonathan</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
-                    <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
                     <p className="text-sm">Verified Purchase</p>
                   </div>
                 </div>
@@ -122,15 +124,15 @@ export default function Home() {
               </div>
               <div className="text-lg leading-8">
                 <p>"I ususally keep my phone togther with my keys in my pocket and that led to some pretty heavy scratchmarks on all of my last phone cases. This one,
-                 besides a barely noticeable scratch on ther corner, <span className="p-0.5 bg-slate-800 text-white">looks brand new after about half a year</span>
-                 . I dig it"</p>
+                  besides a barely noticeable scratch on ther corner, <span className="p-0.5 bg-slate-800 text-white">looks brand new after about half a year</span>
+                  . I dig it"</p>
               </div>
               <div className="flex gap-4 mt-2">
-                <img className="rounded-full h-12 w-12 object-cover" src='/users/user-4.jpg'/>
+                <img className="rounded-full h-12 w-12 object-cover" src='/users/user-4.jpg' />
                 <div className="flex flex-col">
                   <p className="font-semibold">Mike</p>
                   <div className="flex gap-1.5 items-center text-zinc-600">
-                    <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                    <Check className="h-4 w-4 stroke-[3px] text-green-600" />
                     <p className="text-sm">Verified Purchase</p>
                   </div>
                 </div>
@@ -140,8 +142,66 @@ export default function Home() {
         </MaxWidthWrapper>
 
         <div className="pt-16">
-        <Reviews />
+          <Reviews />
         </div>
+      </section>
+
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 tracking tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">Upload your photo
+                <span className="relative px-2 bg-green-600 text-white">your own case</span> now</h2>
+            </div>
+          </div>
+          <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+            <div className='relative flex flex-col items-center md:grid grid-cols-2 gap-40'>
+              <img
+                src='/arrow.png'
+                className='absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0'
+              />
+
+              <div className='relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl'>
+                <img
+                  src='/horse.jpg'
+                  className='rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full'
+                />
+              </div>
+
+              <Phone className='w-60' imgSrc='/horse_phone.jpg' />
+            </div>
+          </div>
+
+          <ul className='mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit'>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              High-quality silicone material
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              Scratch- and fingerprint resistant coating
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />
+              Wireless charging compatible
+            </li>
+            <li className='w-fit'>
+              <Check className='h-5 w-5 text-green-600 inline mr-1.5' />5 year
+              print warranty
+            </li>
+
+            <div className='flex justify-center'>
+              <Link
+                className={buttonVariants({
+                  size: 'lg',
+                  className: 'mx-auto mt-8',
+                })}
+                href='/configure/upload'>
+                Create your case now <ArrowRight className='h-4 w-4 ml-1.5' />
+              </Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
       </section>
     </div>
   );
