9004f7b142dd35a8f3e082f14c83c222c57dfa68
Kaloyan <kthristow@gmail.com>
2024-09-24 21:26:39 +0300

    Create the summary page and the admin page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
 
 export const formatPrice = (price: number) => {
   const formatter = new Intl.NumberFormat('en-US', {
@@ -13,4 +13,35 @@ export const formatPrice = (price: number) => {
   })
 
   return formatter.format(price)
+}
+
+export function constructMetadata({
+  title = 'CaseCobra - custom high-quality phone cases',
+  description = 'Create custom high-quality phone cases in seconds',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@kthristow',
+    },
+    icons,
+    metadataBase: new URL("https://case-ecommerce-one.vercel.app/")
+  }
 }
